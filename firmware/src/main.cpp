ae8c9b7af40aad5c065a362de4ab254588ed547b
M Indraneeli Vardhan <vardhanm0257@gmail.com>
2026-02-13 22:52:55 +0530

    Phase 1: OLED display working and stable

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,12 +1,24 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+
+// Software I2C: SCL = 9, SDA = 8
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(
+    U8G2_R0,
+    /* clock=*/9,
+    /* data=*/8,
+    /* reset=*/U8X8_PIN_NONE
+);
 
 void setup() {
-    Serial.begin(115200);
-    delay(5000);   // give time to open serial monitor
-    Serial.println("Flipper-Lite: Boot OK");
+    u8g2.begin();
+
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.drawStr(10, 30, "Flipper-Lite");
+    u8g2.drawStr(10, 50, "OLED OK");
+    u8g2.sendBuffer();
 }
 
 void loop() {
-    Serial.println("Running...");
-    delay(2000);
 }
